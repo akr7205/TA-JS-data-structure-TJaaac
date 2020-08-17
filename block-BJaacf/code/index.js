526dab304953dddb3be7c7a2f5e9bf0ccb0fdc68
Ankit <iankit17@gmail.com>
2020-08-17 18:45:33 +0530

    add block-BJaacf

---
+++ block-BJaacf/code/index.js
@@ -0,0 +1,19 @@
+// 1. Create an array named numbers and store 5 number values in it
+
+// 2. Calculates the sum of array items and print it to the console using console.log()
+
+// 3. Calculates the average of array items and print it to the console using console.log()
+
+// 4. Find the highest number in the array and print it to the console using console.log()
+
+// 5. Find the lowest number in the array and print it to the console using console.log()
+
+// 6. Find the even numbers in the array and print them to the console using console.log()
+
+// 7. Find the odd numbers in the array and print them to the console using console.log()
+
+// 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
+
+// 9. Log all the element of the array one by one
+
+// 10. Find all the number in the array that is divisible by 3
