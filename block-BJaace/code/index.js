2669dc474cc4038d3bae181b38d4186f7f0af24b
Ankit <iankit17@gmail.com>
2020-08-17 18:45:33 +0530

    add block-BJaace

---
+++ block-BJaace/code/index.js
@@ -0,0 +1,39 @@
+// 1. Declare an empty array and store it in a variable.
+
+// 2. Now check the type of empty array you declared above.
+
+// 3. Create an array named `colors` that contains five different names of colors as strings.
+
+// 4. Access the first color in the array and print it to the console using `console.log()`
+
+// 5. Access the third color in the array and print it to the console using `console.log()`
+
+// 6. Access the last color in the array and print it to the console using `console.log()`
+
+// 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
+
+// 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
+
+// 9. Now, Find the length of the array named `colors`.
+
+// 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
+
+// 11. Add another color to the end of the list.
+
+// 12. Add another color to the beginning of the list.
+
+// 13. Remove the first color from the starting of the array
+
+// 14. Remove the last color from the end of list, and then print the length of the array to the console one more time.
+
+// 15. Remove the second color from the end of list.
+
+// 16. Create a copy of array named colors (hint: spread operator (...) is used to clone an array)
+
+// 17. Write a for loop to iterate through every element in the array and print each element to the console.
+
+// 18. Copying from that loop you just wrote, modify it to print every color's value and every color's index in this format: 3, purple or 0, blue etc.
+
+// 19. Create a variable named lastColor that will always point to the last element of the colors array, no matter how many colors are in the list. (Hint: make use of the array's length property for this!)
+
+// 20. Add a new color `tomato` to the index 45. And check the length of the array. Use `console.log` to print.
