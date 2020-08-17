500aabd3b3fe79a70fc2fe87f208847b355b2dc4
Ankit <iankit17@gmail.com>
2020-08-17 18:45:33 +0530

    add block-BJaacc

---
+++ block-BJaacc/code/index.js
@@ -0,0 +1,25 @@
+// 1. Define a variable named `students` and assign a blank object to it.
+
+// 2. Add a key of the value of variable `studentName` in the object created above with the value of your name.
+
+// 3. Using `console.log` log the value of `studentName` key from the `students` object
+
+// 4. Add a another key of the value of variable `batch` in the object with the value of 16.
+
+// 5. Using `console.log` log the value of `batch` key from the `students` object
+
+// 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
+
+// 7. Using `console.log` print the value of `isAdult` key from the `students` object
+
+// 8. Add a another key of `42` to the object with a value of `The answer to the meaning of life`.
+
+// 9. Using `console.log` print the value of key `42` from the `students` object
+
+// 10. Check the length of object named `students`.
+
+// 11. Can you define a key of `let or var` in any object? Reason.
+
+// 12. Delete the `isAdult` property from the object `students`. Also, print the object before or after deleting the property
+
+// 13. Update the value of the key batch in the object `students` and print it using `console.log`
