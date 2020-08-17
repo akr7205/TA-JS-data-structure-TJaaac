e71d7ee3f02996dfd2c6a9c6d438079545294c14
Ankit <iankit17@gmail.com>
2020-08-17 18:45:34 +0530

    add block-BJaaco

---
+++ block-BJaaco/code/index.js
@@ -0,0 +1,41 @@
+/*
+Use the different way of accessing and assigning a value to the object using `.` or `[]`
+*/
+
+// - Define a variable named `user` and assign a blank object to it.
+
+// - Add a key of `user name` and a value of `Black Panther` to that object. (don't remove the space between user and name)
+
+// - Using `console.log` log the value of `user name` key from the user object.
+
+// - Add a key of the value of variable `batch` in the object with the value of `16`
+
+let batch = 'myBatch';
+
+// - Using the alert function alert the value of the key added above.
+
+// - Add a key of `42` to the object with a value of `The answer to the meaning of life 🧸`.
+
+// - Add a new key named `clothing` and value will be a blank object.
+
+// - Add a property to the value of `clothing` key named `jeans` with the value of 10.
+
+// - Add another key to clothing with key `shirts` with the value 6.
+
+// - Change the `shirt` key value to 12.
+
+// - Access a property named `siblings`. If the property does not exist log `There are no siblings to this user`
+
+// - Add a value of an array to the key `siblings` key. Array will be `["Robb","Ryan"]`
+
+// - Access the first index of the sibling.
+
+// - Add another value to the key `siblings` array `"Bran"`
+
+// - Log the last value of the sibling array
+
+// - Log the length of the `siblings` array
+
+// - Log all the name of sibline using for..of loop
+
+// - Change the last value of `siblings` array to `"John"`
